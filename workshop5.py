1af7be24914eecb9c2dc531abc67223fefc9c487
Adrian Tomalla <adrian.tomalla@novatec-gmbh.de>
2019-05-30 19:32:27 +0200

    no more fake money

--- workshop5.py
+++ workshop5.py
@@ -1,75 +1,82 @@
 """Die Aufgabe Eurobanknoten aus dem Workshop05
     erstellt: 20.05.2019
-    author: ######
+    author: Sarah cutipie kawaiinekogirl
     
     Beschreibungstext
-    
+    Awesome python function to test if eurobanknotes are valid or not
+    follow my dog on insta: @gremljung
 
 """
+
+#hilfsfunktion die die anzahl der stellen einer zahl ermittelt
+def stellenAnzahl(zahl):
+    stelle = 0
+    for x in str(zahl):
+      stelle += 1
+
+    return stelle
+
 
 def test_seriennummer(tup):
     #iterieren durch Elemente des Tupels        
     for var in tup:
-        serien_string = (tup.index(var), var)
-        print(serien_string)
 
+        quer_summe=0
         #Laenge des Strings, zwei Buchstaben am Anfang [0] und [1], Rest sind Ziffern[2] - [11]
-        laenge_string = len(serien_string)
-        buchstabe_erste_stelle = " "
-        buchstabe_zweite_stelle = " "
-        quer_summe = 0
-        
+        laenge_string = len(var)
+ 
+
         #Laenge des Strings muss 12 sein
         if laenge_string == 12:
-            
-            erster_buchstabe = serien_string[0]
-            zweiter_buchstabe = serien_string[1]
-            #erste zwei Stellen muessen Buchstaben sein
-            if type(erster_buchstabe) == str and type(zweiter_buchstabe) == str:
+#            print ("leange okay")
+            erster_buchstabe = var[0]
+            zweiter_buchstabe = var[1]
+
+           
+
+            #erste zwei Stellen muessen Buchstaben sein und der rest nur ziffern
+            if type(erster_buchstabe) == str and type(zweiter_buchstabe) == str and var[2:].isdigit():
             #if buchstabe_erste_stelle == erster_buchstabe and buchstabe_zweite_stelle == zweiter_buchstabe:
                 #ermitteln der Position der ersten beiden Buchstaben mithilfe der ASCII-Tabelle
+                
+                #ascii repreasentation der erste beiden buchstaben
                 pos_erster_buchstabe = ord(erster_buchstabe)
                 pos_zweiter_buchstabe = ord(zweiter_buchstabe)
+                 
+                #ascii repreasentation mit 64 abziehen um die eigentlihce position im alphabet zu bekommen
+                string_erster_buchstabe = str(pos_erster_buchstabe-64 )
+                string_zweiter_buchstabe = str(pos_zweiter_buchstabe-64)
                 
-                print(pos_erster_buchstabe, " ", pos_zweiter_buchstabe)
-                
-                #int in String umwandeln
-                string_erster_buchstabe = str(pos_erster_buchstabe)
-                string_zweiter_buchstabe = str(pos_zweiter_buchstabe)
-                
-                #Iteriere durch den String
-                for zifferBuchstabe in string_erster_buchstabe:
-                    quer_summe = quer_summe + int(zifferBuchstabe)
-                    
-                for zifferBuchstabe in string_zweiter_buchstabe:
-                    quer_summe = quer_summe + int(zifferBuchstabe)
-                    
-                print(quer_summe)
-                
-                #Rest String wird ausgegeben
-                for i in range(2, len(serien_string)):
-                    rest_string = str(serien_string[i])
-                    
-                    #Iteriere durch diesen Reststring
-                    for e in rest_string:
-                        quer_summe = quer_summe + int(e)
 
-                print(quer_summe)
-
-            #Quersummen-String in int umwandeln           
-            int_quer_summe = int(quer_summe)
-            
-            if int_quer_summe / 9 == 7:
-                print("Der String", serien_string, "entspricht einer gültigen Seriennummer.")
+                #mithilfe der hilfsfunktion die anzahl der stellen ermitteln
+                stellen = stellenAnzahl(string_erster_buchstabe)
+                stellen += stellenAnzahl(string_zweiter_buchstabe)
+                
+                #Erste zwei zahlen addieren
+                quer_summe =  int(string_erster_buchstabe)    
+                quer_summe +=  int(string_zweiter_buchstabe)
+               
+               
+           
+                #pruefziffern plus pruefzahl zur quersumme addieren 
+                for i in var[2:]:
+                    quer_summe += int(i)
+                    
+  
+            # ueberpruefen ob der rest der division 7 ergibt und die anzahl der stellen kleiner als 5 ist
+            if quer_summe % 9 == 7 and stellen < 5:
+                print("Der String", var, "entspricht einer gültigen Seriennummer.")
                 
             else:
-                print("Keine gueltige Seriennummer.")
+                print("Der String", var , "Keine gueltige Seriennummer.")
                 
 
         
         else:
-            print("Der String", serien_string, "entspricht keiner gültigen Seriennummer.")
+            print("Der String", var, "entspricht keiner gültigen Seriennummer.")
             
+
+
 
 tuple = ("VAZUI321", "VAZ473965038", "X80402108303", "XA0384560721", "XA0473965058",
                     "VA2237393612", "XA0473965038")
